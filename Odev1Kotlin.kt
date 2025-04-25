4e9ac8c9b7e89cf4d972674d92bf822b9f8481b0
samilsahin <156577196+samilsahin@users.noreply.github.com>
2025-04-25 15:12:27 +0300

    Add files via upload

---
+++ Odev1Kotlin.kt
@@ -0,0 +1,99 @@
+package odev1
+
+
+
+fun main() {
+    val sehir: String = "Ankara"
+    val ulke: String = "Türkiye"
+    val telefon: String = "05551234567"
+    val postaKodu: String = "34000"
+    val email: String = "kullanici@example.com"
+    val meslek: String = "Mühendis"
+    val stokMiktari: Int = 150
+    val musteriAdi: String = "Ayşe Yılmaz"
+    val bakiye: Double = 1250.75
+    val dogumGunu: String = "15/08/1990"
+    val maas: Double = 50000.00
+    val medeniDurum: String = "Evli"
+    val urunYorum: String = "Çok beğendim, tavsiye ederim!"
+    val odemeTarihi: String = "2024-12-31"
+    val odemeTutari: Double = 250.50
+    val siparisAdeti: Int = 3
+    val arabaModeli: String = "Honda Civic"
+    val kitapAdi: String = "Sefiller"
+    val yayinlamaTarihi: String = "1862"
+    val indirimMiktari: Double = 25.00
+    val odaSayisi: Int = 3
+    val enlem: Double = 41.0082
+    val boylam: Double = 28.9784
+    val urunAdi: String = "Akıllı Telefon"
+    val yemekFiyati: Double = 85.99
+    val marka: String = "Samsung"
+    val muzikAdi: String = "Bohemian Rhapsody"
+    val videoSuresi: String = "3:45"
+    val urunPuani: Double = 4.7
+    val resimAdi: String = "tatil_fotografi.jpg"
+    val dosyaFormati: String = "pdf"
+    val renk: String = "Mavi"
+    val renkKodu: String = "#0000FF"
+    val telefonModeli: String = "iPhone 15"
+    val ekranBoyutu: String = "6.1 inç"
+    val agirlik: Double = 0.172
+    val ulusalGun: String = "23 Nisan Ulusal Egemenlik ve Çocuk Bayramı"
+    val tatilGunu: String = "Yılbaşı"
+    val rezervasyonTarihi: String = "2025-07-20"
+    val sokakAdi: String = "Cumhuriyet Caddesi"
+    val otobusHatti: String = "E-5"
+    val kalanDakika: Int = 150
+    val takipKodu: String = "TK789123456"
+    val kuponSuresi: String = "2025-06-30"
+    val kuponKodu: String = "IND15"
+    val faturaAdresi: String = "Örnek Mah. Örnek Sok. No: 123, Örnek İlçe, Örnek İl"
+
+    println("Şehir: $sehir")
+    println("Ülke: $ulke")
+    println("Telefon: $telefon")
+    println("Posta Kodu: $postaKodu")
+    println("Email: $email")
+    println("Meslek: $meslek")
+    println("Stok Miktarı: $stokMiktari")
+    println("Müşteri Adı: $musteriAdi")
+    println("Bakiye: $bakiye")
+    println("Doğum Günü: $dogumGunu")
+    println("Maaş: $maas")
+    println("Medeni Durum: $medeniDurum")
+    println("Ürün Yorumu: $urunYorum")
+    println("Ödeme Tarihi: $odemeTarihi")
+    println("Ödeme Tutarı: $odemeTutari")
+    println("Sipariş Adeti: $siparisAdeti")
+    println("Araba Modeli: $arabaModeli")
+    println("Kitap Adı: $kitapAdi")
+    println("Yayınlama Tarihi: $yayinlamaTarihi")
+    println("İndirim Miktarı: $indirimMiktari")
+    println("Oda Sayısı: $odaSayisi")
+    println("Enlem: $enlem")
+    println("Boylam: $boylam")
+    println("Ürün Adı: $urunAdi")
+    println("Yemek Fiyatı: $yemekFiyati")
+    println("Marka: $marka")
+    println("Müzik Adı: $muzikAdi")
+    println("Video Süresi: $videoSuresi")
+    println("Ürün Puanı: $urunPuani")
+    println("Resim Adı: $resimAdi")
+    println("Dosya Formatı: $dosyaFormati")
+    println("Renk: $renk")
+    println("Renk Kodu: $renkKodu")
+    println("Telefon Modeli: $telefonModeli")
+    println("Ekran Boyutu: $ekranBoyutu")
+    println("Ağırlık: $agirlik")
+    println("Ulusal Gün: $ulusalGun")
+    println("Tatil Günü: $tatilGunu")
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+    println("Sokak Adı: $sokakAdi")
+    println("Otobüs Hattı: $otobusHatti")
+    println("Kalan Dakika: $kalanDakika")
+    println("Takip Kodu: $takipKodu")
+    println("Kupon Süresi: $kuponSuresi")
+    println("Kupon Kodu: $kuponKodu")
+    println("Fatura Adresi: $faturaAdresi")
+}
