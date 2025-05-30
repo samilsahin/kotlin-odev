3030b1eafc675cae93c8cc1b971de3de3a5c131b
samilsahin <156577196+samilsahin@users.noreply.github.com>
2025-05-30 17:33:13 +0300

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/ahmetsamilsahin/bitirmeprojesi/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ahmetsamilsahin/bitirmeprojesi/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.bekirahmetli.bitirmeprojesi
+package com.ahmetsamilsahin.bitirmeprojesi
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
